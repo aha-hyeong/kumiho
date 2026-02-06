a77aab119d21b6aaffa606f7ba07b6694a83c319
aha-hyeong <ahahyeong@gmail.com>
2026-02-07 01:11:40 +0900

    chore: 버전 업데이트

--- backend/internal/handler/system_handler.go
+++ backend/internal/handler/system_handler.go
@@ -30,7 +30,7 @@ type VersionInfo struct {
 	NeedsUpdate    bool   `json:"needs_update"`
 }
 
-const CurrentVersion = "v0.6.0"
+const CurrentVersion = "v0.6.1"
 const GithubRepo = "aha-hyeong/kumiho"
 
 func NewSystemHandler(settingRepo repository.SettingRepository) *SystemHandler {
