cd17721b02a197c546b8f5618860c91e09c954a2
aha-hyeong <ahahyeong@gmail.com>
2026-02-20 17:24:56 +0900

    fix(sse): reduce keepalive to 5s for faster disconnect detection

--- backend/internal/handler/sse_handler.go
+++ backend/internal/handler/sse_handler.go
@@ -67,7 +67,7 @@ func (h *SSEHandler) Handle(c *fiber.Ctx) error {
 		// 현재 접속자 수를 즉시 갱신 (리액트 컴포넌트 마운트 시 초기값 제공)
 		h.hub.TriggerUserCount()
 
-		ticker := time.NewTicker(30 * time.Second)
+		ticker := time.NewTicker(5 * time.Second)
 		defer ticker.Stop()
 
 		for {
